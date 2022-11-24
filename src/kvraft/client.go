3a9380aa8744ff44243eb1775bb57e43f988cbf2
clingfei <1599101385@qq.com>
2022-11-24 09:35:44 +0800

    fix raft

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,6 +3,7 @@ package kvraft
 import (
 	"6.824/labrpc"
 	"sync"
+	"time"
 )
 import "crypto/rand"
 import "math/big"
@@ -33,65 +34,6 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	return ck
 }
 
-func (ck *Clerk) SendGet(key string, ch chan string) {
-	ck.mu.Lock()
-	args := GetArgs{
-		Key:         key,
-		ClientId:    ck.me,
-		SequenceNum: ck.sequenceId,
-	}
-	reply := GetReply{}
-	for {
-		DPrintf("%d send Get to %d key: %v, sequence: %d \n", ck.me, ck.curLeader, args.Key, args.SequenceNum)
-		if !ck.sendGet(ck.curLeader, &args, &reply) || reply.Err == ErrWrongLeader {
-			DPrintf("%d receive Err: %v, value: %v from %d \n", ck.me, reply.Err, reply.Value, ck.curLeader)
-			ck.curLeader = (ck.curLeader + 1) % len(ck.servers)
-			continue
-		}
-		DPrintf("Call succeed receive %d receive Err: %v, value: %v from %d\n", ck.me, reply.Err, reply.Value, ck.curLeader)
-		ck.sequenceId++
-		ck.mu.Unlock()
-		ch <- reply.Value
-		break
-	}
-
-}
-
-func (ck *Clerk) SendPutAppend(key string, value string, op string, ch chan string) {
-	ck.mu.Lock()
-	args := PutAppendArgs{
-		Key:         key,
-		Value:       value,
-		Op:          op,
-		ClientId:    ck.me,
-		SequenceNum: ck.sequenceId,
-	}
-	reply := PutAppendReply{}
-	for {
-		DPrintf("%d send %v to %d, key: %v, value: %v, sequence: %d\n", ck.me, op, ck.curLeader, args.Key, args.Value, args.SequenceNum)
-		if !ck.sendPutAppend(ck.curLeader, &args, &reply) || reply.Err == ErrWrongLeader {
-			DPrintf("%d receive Err: %v from %d \n", ck.me, reply.Err, ck.curLeader)
-			ck.curLeader = (ck.curLeader + 1) % len(ck.servers)
-			continue
-		}
-		DPrintf("Call succeed, %d receive Err: %v from %d \n", ck.me, reply.Err, ck.curLeader)
-		ck.sequenceId++
-		ck.mu.Unlock()
-		ch <- ""
-		break
-	}
-}
-
-func (ck *Clerk) sendGet(server int, args *GetArgs, reply *GetReply) bool {
-	ok := ck.servers[server].Call("KVServer.Get", args, reply)
-	return ok
-}
-
-func (ck *Clerk) sendPutAppend(server int, args *PutAppendArgs, reply *PutAppendReply) bool {
-	ok := ck.servers[server].Call("KVServer.PutAppend", args, reply)
-	return ok
-}
-
 //
 // fetch the current value for a key.
 // returns "" if the key does not exist.
@@ -106,9 +48,18 @@ func (ck *Clerk) sendPutAppend(server int, args *PutAppendArgs, reply *PutAppend
 //
 func (ck *Clerk) Get(key string) string {
 	// You will have to modify this function.
-	ch := make(chan string)
-	go ck.SendGet(key, ch)
-	return <-ch
+	args := &GetArgs{key, ck.me, ck.sequenceId}
+	ck.sequenceId++
+	for {
+		for _, srv := range ck.servers {
+			var reply GetReply
+			ok := srv.Call("KVServer.Get", args, &reply)
+			if ok && reply.Err != ErrWrongLeader {
+				return reply.Value
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 //
@@ -123,12 +74,21 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
-	ch := make(chan string)
+	args := &PutAppendArgs{key, value, op, ck.me, ck.sequenceId}
+	ck.sequenceId++
 	//A Clerk sometimes doesn't know which kvserver is the Raft leader.
 	//If the Clerk sends an RPC to the wrong kvserver, or if it cannot reach the kvserver,
 	//the Clerk should re-try by sending to a different kvserver.
-	go ck.SendPutAppend(key, value, op, ch)
-	<-ch
+	for {
+		for _, srv := range ck.servers {
+			var reply PutAppendReply
+			ok := srv.Call("KVServer.PutAppend", args, &reply)
+			if ok && reply.Err != ErrWrongLeader {
+				return
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
